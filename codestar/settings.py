17ee9a7c1d9b7bb0ad4fdbdb12a518efabc569ee
Christo107 <christopher.odwyer82@gmail.com>
2023-05-31 14:37:04 +0000

    Updated settings.py file to connect to database

--- codestar/settings.py
+++ codestar/settings.py
@@ -10,15 +10,16 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+import dj_database_url
+if os.path.isfile("env.py"):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-import os
-import dj_database_url
-if os.path.isfile('env.py'):
-    import env
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -86,8 +87,8 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 # }
    
 DATABASES = {
-     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
- }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 
 # Password validation
