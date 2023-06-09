a8f20dc90ce5f3479c2dd5077f051acf5e229767
Christo107 <christopher.odwyer82@gmail.com>
2023-06-09 15:15:23 +0000

    Fix 405 error

--- blog/views.py
+++ blog/views.py
@@ -7,8 +7,8 @@ from .forms import CommentForm
 
 class PostList(generic.ListView):
     model = Post
-    queryset = Post.objects.filter(status=1).order_by('-created_on')
-    template_name = 'index.html'
+    queryset = Post.objects.filter(status=1).order_by("-created_on")
+    template_name = "index.html"
     paginate_by = 6
 
 
@@ -23,35 +23,33 @@ class PostDetail(View):
             liked = True
 
         return render(
-                request,
-                "post_detail.html",
-                {
-
-                    "post": post,
-                    "comments": comments,
-                    "commented": False,
-                    "liked": liked,
-                    'comment_form': CommentForm()
-                },
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "commented": False,
+                "liked": liked,
+                "comment_form": CommentForm()
+            },
         )
+    
+    def post(self, request, slug, *args, **kwargs):
 
-        def post(self, request, slug, *args, **kwargs):
-            queryset = Post.objects.filter(status=1)
-            post = get_object_or_404(queryset, slug=slug)
-            comments = post.comments.filter(approved=True).order_by("-created_on")
-            liked = False
-            if post.likes.filter(id=self.request.user.id).exists():
-                liked = True
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by("-created_on")
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
 
-            comment_form = CommentForm(data=request.POST)
-
+        comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
             comment_form.instance.email = request.user.email
             comment_form.instance.name = request.user.username
             comment = comment_form.save(commit=False)
             comment.post = post
             comment.save()
-
         else:
             comment_form = CommentForm()
 
@@ -69,12 +67,11 @@ class PostDetail(View):
 
 
 class PostLike(View):
-
-    def get(self, request, slug):
+    
+    def post(self, request, slug, *args, **kwargs):
         post = get_object_or_404(Post, slug=slug)
         if post.likes.filter(id=request.user.id).exists():
             post.likes.remove(request.user)
-
         else:
             post.likes.add(request.user)
 
